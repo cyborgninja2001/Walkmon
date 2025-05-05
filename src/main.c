ec7ff7b516b8087b7a33624d2ce50dec5e52ab63
Cyborg_Ninja <cyborgninja2001@gmail.com>
2025-05-04 22:33:05 -0300

    testing load_rom & eeprom

--- src/main.c
+++ src/main.c
@@ -5,6 +5,18 @@
 #include "memory.h"
 
 int main(int argc, char *argv[]) {
+    bool loaded_rom = load_rom(argv[1]);
+    if (loaded_rom) {
+        printf("ROM LOADED SUCCESSFULLY!\n");
+    } else {
+        printf("COULDN'T LOAD ROM\n");
+    }
+    bool loaded_eeprom = eeprom_load(argv[2]);
+    if (loaded_eeprom) {
+        printf("EEPROM LOADED SUCCESSFULLY!\n");
+    } else {
+        printf("COULDN'T LOAD EEPROM\n");
+    }
     cpu_reset();
     cpu_debug();
     return 0;
