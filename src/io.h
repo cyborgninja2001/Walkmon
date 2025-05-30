1593d1af3f86bd1eee6b4b7e0abef61a673079fe
Cyborg_Ninja <cyborgninja2001@gmail.com>
2025-05-30 11:52:21 -0300

    defining macros for io ports

---
+++ src/io.h
@@ -0,0 +1,35 @@
+#ifndef IO_H
+#define IO_H
+
+// Port 1
+#define PDR1  0xFFD4
+#define PCR1  0xFFE4
+#define PUCR1 0xFFE0
+#define PMR1  0xFFC0
+
+// Port 3
+#define PDR3  0xFFD6
+#define PCR3  0xFFE6
+#define PUCR3 0xFFE1
+#define PMR3  0xFFC2
+
+// Port 8
+#define PDR8  0xFFDB
+#define PCR8  0xFFEB
+#define PUCR8 0xF086
+
+// Port 9
+#define PDR9  0xFFDC
+#define PCR9  0xFFEC
+#define PODR9 0xF08C
+#define PUCR9 0xF087
+
+// Port B
+#define PDRB 0xFFDE
+#define PMRB 0xFFCA
+
+// Input/Output Data Inversion
+#define SPCR 0xFF91
+#define PFCR 0xF085
+
+#endif
